af1fc26d745858f7a12d03b81bd12156d778fa64
diptesh03 <diptesh03@gmail.com>
2017-10-24 16:58:54 +0530

    no message

---
+++ Samara/Models/SiteDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Samara.Models
+{
+    public class SiteDetail
+    {
+        public int SiteID { get; set; }
+
+        public string Name { get; set; }
+
+        public string SiteName { get; set; }
+
+       
+    }
+}
